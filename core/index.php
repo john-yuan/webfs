ba0a856b14be03678ec105602e3d3a0b9b0573fe
John Yuan <John.YCQ@outlook.com>
2019-05-24 22:04:32 +0800

    Set error reporting level.

--- core/index.php
+++ core/index.php
@@ -7,3 +7,9 @@ require_once __DIR__ . '/functions.php';
 require_once __DIR__ . '/error_handlers.php';
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/check_config.php';
+
+if (config('debug')) {
+    error_reporting(E_ALL);
+} else {
+    error_reporting(0);
+}
